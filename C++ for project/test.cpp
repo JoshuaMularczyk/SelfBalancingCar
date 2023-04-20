1b8e0d3fb3ea4ece9007495d646ec3e9f691bb64
DESKTOP-6H04RCO\jcmul <jcmularczyk@gmail.com>
2023-04-20 11:15:56 -0700

    Adding of initial files

---
+++ C++ for project/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int name = 0;
+    std::cout << "what is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << name << std::endl;
+
+    return 0;
+}
